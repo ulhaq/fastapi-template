7924c87df9e36855a492c9f7726a138c41d4a7b3
ulhaq <ullhaq@hotmail.com>
2025-06-02 20:35:42 +0200

    change response model

--- src/routers/user.py
+++ src/routers/user.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 from src.core.dependencies import get_current_user
 from src.core.security import oauth2_scheme
 from src.models.user import User
-from src.schemas.user import UserBase, UserIn, UserOut
+from src.schemas.user import UserIn, UserOut
 from src.services.user import UserService
 
 router = APIRouter()
@@ -16,10 +16,10 @@ def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
 
 
-@router.post("/users", response_model=UserBase)
+@router.post("/users", response_model=UserOut)
 def create(
     user_in: UserIn,
     service: Annotated[UserService, Depends(UserService)],
     _: Annotated[str, Depends(oauth2_scheme)],
-) -> UserBase:
+) -> UserOut:
     return service.create(user_in)
