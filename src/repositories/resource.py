54cfe36461f82fad89eb1de99cb4633020e6d2f5
ulhaq <ullhaq@hotmail.com>
2025-06-03 20:33:42 +0200

    add exists method

--- src/repositories/resource.py
+++ src/repositories/resource.py
@@ -1,6 +1,6 @@
 from typing import Generic, Sequence, Type, TypeVar
 
-from sqlalchemy import select
+from sqlalchemy import exists, select
 
 from src.core.database import Base
 from src.repositories.base import BaseRepository
@@ -14,33 +14,27 @@ class ResourceRepository(Generic[ModelType], BaseRepository):
         self.model = model
 
     def get(self, identifier: int) -> ModelType | None:
-        """
-        Get a record by primary key
-        """
         return self.db.get(self.model, identifier)
 
     def get_or_fail(self, identifier: int) -> ModelType:
-        """
-        Get a record by primary key or fail if not found
-        """
         return self.db.get_one(self.model, identifier)
 
+    def exists(self, identifier: int) -> bool:
+        return self.db.execute(
+            select(
+                exists(1).where(
+                    self.model.id == identifier  # type: ignore[attr-defined]
+                )
+            )
+        ).scalar_one()
+
     def get_all(self) -> Sequence[ModelType]:
-        """
-        Get all records
-        """
         return self.db.scalars(select(self.model)).all()
 
     def filter_by(self, **kwargs) -> Sequence[ModelType]:
-        """
-        Filter records using keyword arguments
-        """
         return self.db.scalars(select(self.model).filter_by(**kwargs)).all()
 
     def create(self, **kwargs) -> ModelType:
-        """
-        Create and persist a new record
-        """
         instance = self.model(**kwargs)
         self.db.add(instance)
         self.db.commit()
@@ -48,9 +42,6 @@ class ResourceRepository(Generic[ModelType], BaseRepository):
         return instance
 
     def update(self, model: ModelType, **kwargs) -> ModelType:
-        """
-        Update the instance with provided kwargs
-        """
         for attr, value in kwargs.items():
             setattr(model, attr, value)
         self.db.add(model)
@@ -59,8 +50,5 @@ class ResourceRepository(Generic[ModelType], BaseRepository):
         return model
 
     def delete(self, model: ModelType) -> None:
-        """
-        Delete the instance
-        """
         self.db.delete(model)
         self.db.commit()
