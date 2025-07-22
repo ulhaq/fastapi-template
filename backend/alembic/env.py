d3c2f7210a59fd69e08ed8610bb224949ed9da25
ulhaq <ullhaq@hotmail.com>
2025-07-22 22:39:22 +0200

    fix init_db

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,16 +1,16 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.getcwd())
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-
-from src.core.database import Base
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 from src import models
+from src.core.database import Base
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -75,9 +75,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
