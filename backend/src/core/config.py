945af0750e22e676044e81ae3fb822324b9fc958
ulhaq <ullhaq@hotmail.com>
2026-03-02 20:16:17 +0100

    ignore extra env vars

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -5,7 +5,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="./.env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file="./.env", env_file_encoding="utf-8", extra="ignore"
+    )
 
     app_name: str = ""
     app_url: str = "http://localhost"
