8daf07ad56e798ebc01b8ef35016789bfef6d4f2
ulhaq <ullhaq@hotmail.com>
2026-05-01 00:11:18 +0200

    add linter formatter

---
+++ frontend/eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import vueParser from 'vue-eslint-parser'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'playwright-report/', 'test-results/'],
+  },
+]
